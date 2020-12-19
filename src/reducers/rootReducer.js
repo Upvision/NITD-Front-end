cf9c9cb7132e925b29b3d1646a58f66701ecf1e5
mshahanwaz <mohdshahanwaz55@gmail.com>
2020-12-19 22:21:47 +0530

    General Modifications

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,5 @@
-import { combineReducers } from 'redux';
-import settings from './settings';
+import { combineReducers } from "redux";
+import settings from "./settings";
 export default combineReducers({
-    settings
+  settings,
 });
